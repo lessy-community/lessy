1c766c1fc4c84c36c660fd5adaa4a6003fb1c7bb
Marien Fressinaud <dev@marienfressinaud.fr>
2016-12-28 21:18:11 +0100

    tec: #17 Configure email handling for dev and test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,10 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :letter_opener
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
