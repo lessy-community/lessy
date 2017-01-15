875c4dc266ce0952059edc3387739ba7cb9ce2ab
Marien Fressinaud <dev@marienfressinaud.fr>
2017-01-15 21:43:07 +0100

    tec: Improve NotFound page's design

--- client/src/components/pages/NotFound.vue
+++ client/src/components/pages/NotFound.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="not-found-page">
     <h1>Page not found</h1>
-    <router-link to="/">Home page</router-link>
+    <p>It looks like this page doesn't exist…</p>
+    <div>
+      <router-link to="/">Home page</router-link>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'not-found-page',
-}
+  export default {
+
+    name: 'not-found-page',
+
+  }
 </script>
+
+<style>
+
+  .container > .not-found-page {
+    padding-top: 42px;
+    font-size: 2rem;
+    text-align: center;
+  }
+
+</style>
