bbf91514c1932edc109d3121a4c08328a73b9d5b
Marien Fressinaud <dev@marienfressinaud.fr>
2017-01-28 21:12:52 +0100

    tec: Move lib files in app/lib
    As recommended in https://github.com/rails/rails/issues/13142 and
    http://collectiveidea.com/blog/archives/2016/07/22/solutions-to-potential-upgrade-problems-in-rails-5/

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,5 @@ module ProjectZero
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    config.autoload_paths << Rails.root.join('lib')
   end
 end
