16ff15a266c1990e2079000ede947e23b73c90e3
Marien Fressinaud <dev@marienfressinaud.fr>
2017-06-24 10:28:16 +0200

    tec: Provide Chart.js

---
+++ client/src/components/LineChart.vue
@@ -0,0 +1,13 @@
+<script>
+  import { Line } from 'vue-chartjs'
+
+  export default Line.extend({
+    props: ['data', 'options'],
+    mounted () {
+      this.renderChart(
+        this.data,
+        { responsive: true, maintainAspectRatio: false }
+      )
+    }
+  })
+</script>
