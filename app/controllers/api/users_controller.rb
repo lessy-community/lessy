9ac35c1fee30a5d37198f8f6e65019d50f836ebd
Marien Fressinaud <dev@marienfressinaud.fr>
2018-09-08 19:13:42 +0200

    tec: Refactor require_registration_enabled

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,14 +2,10 @@ class Api::UsersController < ApiController
   skip_before_action :require_login, only: [:create]
   skip_before_action :require_tos_accepted, only: %i[create show destroy accept_tos]
 
+  before_action :require_registration_enabled, only: :create
   before_action :require_sudo, only: :destroy
 
   def create
-    unless Flipper.enabled? :feature_registration
-      render_error ApiErrors::RegistrationDisabled.new, :forbidden
-      return
-    end
-
     @user = User.create!(create_user_params)
     @token = @user.token(expiration: 1.day.from_now)
     render status: :created
@@ -29,6 +25,11 @@ class Api::UsersController < ApiController
   end
 
 private
+
+  def require_registration_enabled
+    registration_disabled = !Flipper.enabled?(:feature_registration)
+    render_error ApiErrors::RegistrationDisabled.new, :forbidden if registration_disabled
+  end
 
   def create_user_params
     fetch_resource_params(:user, [:email])
