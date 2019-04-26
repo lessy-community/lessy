lessy-community/lessy

commit ecd11be6cf88daf0adff7be6ee18208e0279b24a

tec: Update Dockerfiles
Dockerfile doesn't build without nodejs-npm package since `nodejs` no
longer comes with `npm` in Alpine 3.6.

Dockerfile
@@ -13,9 +13,9 @@ ENV RAILS_LOG_TO_STDOUT true
 
 RUN apk add --no-cache \
       nodejs \
+      nodejs-npm \
       postgresql-client \
-      tzdata \
-  && npm install npm@6 -g \
+      tzdata
   && apk --update add --virtual build-dependencies \
       build-base \
       ruby-dev \

Dockerfile.dev
@@ -7,9 +7,9 @@ WORKDIR /app/
 
 RUN apk add --no-cache \
       nodejs \
+      nodejs-npm \
       postgresql-client \
-      tzdata \
-  && npm install npm@6 -g
+      tzdata
 RUN apk --update add --virtual build-dependencies \
       build-base \
       ruby-dev \

commit 5cd527f63b6799950667d7e07174e324afd69103

fix: Fix production Dockerfile
Bug introduced in ecd11be6cf88daf0adff7be6ee18208e0279b24a

Dockerfile
@@ -15,7 +15,7 @@ RUN apk add --no-cache \
       nodejs \
       nodejs-npm \
       postgresql-client \
-      tzdata
+      tzdata \
   && apk --update add --virtual build-dependencies \
       build-base \
       ruby-dev \