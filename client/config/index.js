343f4c418d33e30b4fe830c216d826cd6ee4e497
Marien Fressinaud <dev@marienfressinaud.fr>
2019-05-05 18:02:45 +0200

    Update Webpack configuration
    This commit is quite heavy but I didn't code any of it. It's just a
    copy/paste from the Vue template repository. I just had to adapt the
    loading of scss variables.
    
    For now it is broken because of the wrong `require` paths.
    
    Reference: https://github.com/vuejs-templates/webpack/

--- client/config/index.js
+++ client/config/index.js
@@ -18,6 +18,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
+    host: '0.0.0.0', // can be overwritten by process.env.HOST
     port: 8080,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
@@ -31,11 +32,31 @@ module.exports = {
         ws: true,
       },
     },
-    // CSS Sourcemaps off by default because relative paths are "buggy"
-    // with this option, according to the CSS-Loader README
-    // (https://github.com/webpack/css-loader#sourcemaps)
-    // In our experience, they generally work as expected,
-    // just be aware of this issue when enabling this option.
-    cssSourceMap: false
+
+    autoOpenBrowser: false,
+    errorOverlay: true,
+    notifyOnErrors: true,
+    poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
+
+    // If true, your code will be linted during bundling and
+    // linting errors and warnings will be shown in the console.
+    useEslint: true,
+    // If true, eslint errors and warnings will also be shown in the error overlay
+    // in the browser.
+    showEslintErrorsInOverlay: false,
+
+    /**
+     * Source Maps
+     */
+
+    // https://webpack.js.org/configuration/devtool/#development
+    devtool: 'cheap-module-eval-source-map',
+
+    // If you have problems debugging vue-files in devtools,
+    // set this to false - it *may* help
+    // https://vue-loader.vuejs.org/en/options.html#cachebusting
+    cacheBusting: true,
+
+    cssSourceMap: true
   }
 }
--- client/src/components/App/App.vue
+++ client/src/components/App/App.vue
@@ -3,3 +3,7 @@
     <router-view></router-view>
   </div>
 </template>
+
+<style lang="scss">
+    @import 'src/styles/app';
+</style>
