340fbeb22322765a9af4e2b00779208f52d00b7b
Marien Fressinaud <dev@marienfressinaud.fr>
2019-05-05 18:01:16 +0200

    Update backend dependencies

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -4,33 +4,33 @@ FactoryBot.define do
     user
 
     trait :newed do
-      state 'newed'
-      started_at nil
-      paused_at nil
-      due_at nil
-      finished_at nil
+      state { 'newed' }
+      started_at { nil }
+      paused_at { nil }
+      due_at { nil }
+      finished_at { nil }
     end
 
     trait :started do
-      state 'started'
+      state { 'started' }
       started_at { 15.days.ago }
-      paused_at nil
+      paused_at { nil }
       due_at { 15.days.from_now }
-      finished_at nil
+      finished_at { nil }
     end
 
     trait :paused do
-      state 'paused'
+      state { 'paused' }
       started_at { 30.days.ago }
       paused_at { 15.days.ago }
       due_at { 15.days.from_now }
-      finished_at nil
+      finished_at { nil }
     end
 
     trait :finished do
-      state 'finished'
+      state { 'finished' }
       started_at { 30.days.ago }
-      paused_at nil
+      paused_at { nil }
       due_at { 15.days.ago }
       finished_at { 15.days.ago }
     end
--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,42 +1,42 @@
 FactoryBot.define do
   factory :task, traits: [:newed] do
-    label "My task"
+    label { 'My task' }
     user
     sequence :order
 
     trait :newed do
-      state 'newed'
-      started_at nil
-      planned_at nil
-      finished_at nil
-      abandoned_at nil
+      state { 'newed' }
+      started_at { nil }
+      planned_at { nil }
+      finished_at { nil }
+      abandoned_at { nil }
     end
 
     trait :started do
-      state 'started'
+      state { 'started' }
       started_at { 15.days.ago }
-      planned_at nil
-      finished_at nil
-      abandoned_at nil
+      planned_at { nil }
+      finished_at { nil }
+      abandoned_at { nil }
     end
 
     trait :planned do
-      state 'planned'
+      state { 'planned' }
       planned_at { 15.days.from_now }
-      finished_at nil
-      abandoned_at nil
+      finished_at { nil }
+      abandoned_at { nil }
     end
 
     trait :finished do
-      state 'finished'
+      state { 'finished' }
       planned_at { 15.days.ago }
       finished_at { 5.days.ago }
-      abandoned_at nil
+      abandoned_at { nil }
     end
 
     trait :abandoned do
-      state 'abandoned'
-      finished_at nil
+      state { 'abandoned' }
+      finished_at { nil }
       abandoned_at { 5.days.ago }
     end
   end
