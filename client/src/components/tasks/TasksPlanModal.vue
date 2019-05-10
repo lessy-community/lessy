lessy-community/lessy

commit 44003af2a6f9bf0f2f2adab632ff948b9902615d

Redesign the task creation form

client/src/components/tasks/TasksPlanModal.vue
@@ -9,10 +9,10 @@
     </p>
 
     <task-create-form
-      :plannedAt="plannedAt"
-      :onCancel="() => { this.$emit('close') }"
-      :onSuccess="() => { this.$emit('close') }"
+      main
       autoFocus
+      :plannedAt="plannedAt"
+      @success="this.$emit('close')"
     ></task-create-form>
 
     <ly-section v-if="tasks.length > 0" :title="$t('tasks.modals.orPickFromBacklog')">

client/src/components/Ly/LyForm/LyFormGroup.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="['ly-form-group', `ly-form-group-${type}`]">
+  <div :class="['ly-form-group', `ly-form-group-${type}`, { stack }]">
     <slot></slot>
   </div>
 </template>
@@ -8,6 +8,7 @@
   export default {
     props: {
       type: { type: String, default: 'input' },
+      stack: { type: Boolean },
     },
   }
 </script>
@@ -25,6 +26,18 @@
       display: block;
       width: 100%;
       margin-bottom: .5rem;
+    }
+  }
+
+  .ly-form-group.stack {
+    display: flex;
+
+    .ly-form-input {
+      flex-grow: 1;
+    }
+
+    * + * {
+      margin-left: .5rem;
     }
   }
 </style>

client/src/components/tasks/TaskCreateForm.vue
@@ -1,6 +1,6 @@
 <template>
   <ly-form @submit="create" size="fluid" :error="getErrors()">
-    <ly-form-group>
+    <ly-form-group stack>
       <ly-form-input
         type="text"
         name="label"
@@ -12,19 +12,19 @@
         required
         :placeholder="$t('tasks.createForm.placeholder')"
       ></ly-form-input>
-    </ly-form-group>
 
-    <ly-form-group type="actions">
-      <ly-button type="primary" submit>
+      <ly-button
+        v-if="created"
+        class="success"
+        icon="check"
+        :type="main ? 'primary' : 'default'"
+        submit
+      >
+        {{ $t('tasks.createForm.created') }}
+      </ly-button>
+      <ly-button v-else :type="main ? 'primary' : 'default'" submit>
         {{ $t('tasks.createForm.submit') }}
       </ly-button>
-      <ly-button v-if="onCancel" @click="onCancel">
-        {{ $t('tasks.createForm.cancel') }}
-      </ly-button>
-      <span :class="['text-success', { show: created }]">
-        <ly-icon name="check"></ly-icon>
-        {{ $t('tasks.createForm.created') }}
-      </span>
     </ly-form-group>
   </ly-form>
 </template>
@@ -36,13 +36,12 @@
     mixins: [ErrorsHandler],
 
     props: {
-      'onSuccess': { type: Function },
-      'onCancel': { type: Function },
-      'autoFocus': { type: Boolean },
-      'plannedAt': { type: Object },
-      'finishedAt': { type: Object },
-      'projectId': { type: Number },
-      'show-warning': { type: Boolean },
+      plannedAt: { type: Object },
+      finishedAt: { type: Object },
+      projectId: { type: Number },
+      autoFocus: { type: Boolean },
+      showWarning: { type: Boolean },
+      main: { type: Boolean },
     },
 
     data () {
@@ -54,6 +53,10 @@
 
     methods: {
       create () {
+        if (this.createdTimeout) {
+          clearTimeout(this.createdTimeout)
+        }
+
         this.$store
           .dispatch('tasks/create', {
             label: this.label,
@@ -66,9 +69,9 @@
             this.label = ''
             this.cleanErrors()
             this.created = true
-            setTimeout(() => { this.created = false }, 5000)
+            this.createdTimeout = setTimeout(() => { this.created = false }, 2000)
             labelInput && labelInput.focus()
-            this.onSuccess && this.onSuccess()
+            this.$emit('success')
           })
           .catch((failure) => {
             this.setFailureErrors(failure)
@@ -86,13 +89,11 @@
 </script>
 
 <style lang="scss" scoped>
-  .text-success {
-    opacity: 0;
+  .ly-button {
+    transition: color .2s ease-in-out;
 
-    transition: opacity .2s ease-in-out;
-
-    &.show {
-      opacity: 1;
+    &.ly-button-default.success {
+      color: $ly-color-green-70;
     }
   }
 </style>

client/src/components/tasks/TasksBacklogPage.vue
@@ -6,7 +6,7 @@
     </app-header>
 
     <ly-section :title="$tc('tasks.backlogPage.tasksNumber', tasks.length, { count: tasks.length })">
-      <task-create-form :show-warning="tasks.length > 30"></task-create-form>
+      <task-create-form main :show-warning="tasks.length > 30"></task-create-form>
       <task-list :tasks="tasks"></task-list>
     </ly-section>
   </app-page>

client/src/locales/en/messages.js
@@ -398,7 +398,7 @@ export default {
 
     createForm: {
       cancel: 'Cancel',
-      created: 'Created!',
+      created: 'Created',
       submit: 'Create',
       warningTooMany: 'You might face difficulties to achieve all your tasks if you try to do too much.',
       placeholder: 'Walk the dog',

client/src/locales/fr/messages.js
@@ -393,7 +393,7 @@ export default {
 
     createForm: {
       cancel: 'Annuler',
-      created: 'Créée !',
+      created: 'Créée',
       submit: 'Créer',
       warningTooMany: 'Vous pourriez rencontrer des difficultés pour mener à bien les tâches si vous tentez d’en entreprendre un trop grand nombre.',
       placeholder: 'Promener le chien',

commit 6e5b81cdf19c51c30decc2ad8013b5b022c08d10

fix: Close plan modal when creating tasks
Bug introduced in 44003af2a6f9bf0f2f2adab632ff948b9902615d

client/src/components/tasks/TasksPlanModal.vue
@@ -12,7 +12,7 @@
       main
       autoFocus
       :plannedAt="plannedAt"
-      @success="this.$emit('close')"
+      @success="$emit('close')"
     ></task-create-form>
 
     <ly-section v-if="tasks.length > 0" :title="$t('tasks.modals.orPickFromBacklog')">